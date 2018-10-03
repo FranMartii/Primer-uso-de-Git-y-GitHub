b012a4e174fd8a8d366b79a7ee26462b6c56cd3a
Francisco Martínez <fran711993@gmail.com>
2018-10-03 13:37:34 +0200

    Guarda fichero en repositorio

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,5 @@
 public class Prueba{
 	public static void main(String args[]){
-	 System.out.println("Francisco Martinez Martinez");
-	 System.out.println("León");
+	 System.out.println("Francisco Martinez Martinez\nCiudad: León\nLocalidad: León");
 	}
 }
