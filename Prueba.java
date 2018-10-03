bbedb777a647d8d31471bb8a4359789dbc8a75bb
Francisco Martínez <fran711993@gmail.com>
2018-10-03 14:06:39 +0200

    Añadir linea con año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba{
 	public static void main(String args[]){
-	 System.out.println("Francisco Martinez Martinez.\nCiudad: León.\nLocalidad: León.");
+	 System.out.println("Francisco Martinez Martinez.\nCiudad: León.\nLocalidad: León.\nAño de Nacimiento: 1993.");
 	}
 }
